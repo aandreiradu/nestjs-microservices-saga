4e84c31851de3ea30285a4c65555a61954268648
Andrei Radu <raduandrei697@gmail.com>
2024-09-16 23:45:16 +0300

    inventory refactor

--- apps/inventory/src/inventory.module.ts
+++ apps/inventory/src/inventory.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InventoryController } from './inventory.controller';
 import { InventoryService } from './inventory.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [InventoryController],
   providers: [InventoryService],
 })
--- apps/inventory/src/main.ts
+++ apps/inventory/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { InventoryModule } from './inventory.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(InventoryModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    InventoryModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672'],
+        queue: 'inventory-queue',
+      },
+    },
+  );
+
+  await app.listen();
 }
 bootstrap();
