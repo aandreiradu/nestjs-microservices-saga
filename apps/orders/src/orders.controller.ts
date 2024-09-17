1a8d3c24a9db0a5f379abe5f138f7abd33be67dc
Andrei Radu <raduandrei697@gmail.com>
2024-09-17 23:39:08 +0300

    refactor: fetch order status service

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -18,38 +18,11 @@ import { ORDERS_STATUSES } from '@app/common/constants/services';
 @Controller()
 export class OrdersController {
   private readonly logger: Logger = new Logger(OrdersController.name);
-  private ordersStatuses = new Map();
   constructor(private readonly ordersService: OrdersService) {}
 
   @EventPattern('create_order')
-  async handleOrderPlaced(@Payload() data: any, @Ctx() context: RmqContext) {
-    try {
-      const orderData = JSON.parse(data) as PlaceOrderDTO;
-
-      this.ordersStatuses.set(orderData.orderId, ORDERS_STATUSES.PROCESSING);
-
-      const stockAvailable = await this.ordersService.checkInventory(orderData);
-
-      if (!stockAvailable) {
-        this.ordersStatuses.set(
-          orderData.orderId,
-          ORDERS_STATUSES.OUT_OF_STOCK,
-        );
-        return true;
-      }
-
-      this.ordersStatuses.set(
-        orderData.orderId,
-        ORDERS_STATUSES.STOCK_CONFIRMED,
-      );
-
-      return true;
-    } catch (error) {
-      console.error('failed to process order_placed event');
-      throw new InternalServerErrorException(
-        'failed to process order_placed event',
-      );
-    }
+  async handleOrderPlaced(@Payload() data: any) {
+    return this.ordersService.handleOrderPlaced(data);
   }
 
   @MessagePattern('fetch_orderStatus')
@@ -58,20 +31,7 @@ export class OrdersController {
       this.logger.debug(
         `Processing fetch_orderStatus for orderId ${JSON.stringify(orderId)}`,
       );
-      const existingOrder = this.ordersStatuses.get(orderId);
-
-      if (!existingOrder) {
-        this.logger.warn(`Order with id ${orderId} was not found`);
-        return {
-          isSuccess: false,
-          message: 'Order not found',
-        };
-      }
-
-      return {
-        isSuccess: true,
-        orderStatus: existingOrder,
-      };
+      return this.ordersService.fetchOrderStatus(orderId);
     } catch (error) {
       this.logger.error(`Failed to fetch order status for orderId ${orderId}`);
       this.logger.error(JSON.stringify(error));
--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,21 +1,75 @@
 import {
   Inject,
   Injectable,
+  InternalServerErrorException,
   Logger,
   ServiceUnavailableException,
 } from '@nestjs/common';
 import { lastValueFrom, timeout } from 'rxjs';
 import { ClientProxy } from '@nestjs/microservices';
 import { PlaceOrderDTO } from './dto/order.dto';
+import { ORDERS_STATUSES } from '@app/common/constants/services';
 
 @Injectable()
 export class OrdersService {
   private readonly logger: Logger = new Logger(OrdersService.name);
+  private ordersStatuses = new Map();
 
   constructor(
     @Inject('INVENTORY_SERVICE') private readonly inventoryClient: ClientProxy,
   ) {}
 
+  async handleOrderPlaced(order: string): Promise<boolean> {
+    try {
+      const orderData = JSON.parse(order) as PlaceOrderDTO;
+      this.ordersStatuses.set(orderData.orderId, ORDERS_STATUSES.PROCESSING);
+
+      const stockAvailable = await this.checkInventory(orderData);
+
+      if (!stockAvailable) {
+        this.ordersStatuses.set(
+          orderData.orderId,
+          ORDERS_STATUSES.OUT_OF_STOCK,
+        );
+        return true;
+      }
+
+      this.ordersStatuses.set(
+        orderData.orderId,
+        ORDERS_STATUSES.STOCK_CONFIRMED,
+      );
+
+      return true;
+    } catch (error) {
+      console.error('failed to process order_placed event');
+      throw new InternalServerErrorException(
+        'failed to process order_placed event',
+      );
+    }
+  }
+
+  async fetchOrderStatus(orderId: string) {
+    try {
+      const existingOrder = this.ordersStatuses.get(orderId);
+      if (!existingOrder) {
+        this.logger.warn(`Order with id ${orderId} was not found`);
+        return {
+          isSuccess: false,
+          message: 'Order not found',
+        };
+      }
+
+      return {
+        isSuccess: true,
+        orderStatus: existingOrder,
+      };
+    } catch (error) {
+      this.logger.error(`Failed to fetch order status for orderId ${orderId}`);
+      this.logger.error(JSON.stringify(error));
+
+      throw new InternalServerErrorException('Failed to retrieve order status');
+    }
+  }
   async checkInventory(order: PlaceOrderDTO) {
     try {
       const response = await lastValueFrom<boolean>(
