8b302b079b611e59c93060b6f7e804a94ab329f9
Andrei Radu <raduandrei697@gmail.com>
2024-09-25 23:51:15 +0300

    clup

--- apps/inventory/src/inventory.controller.ts
+++ apps/inventory/src/inventory.controller.ts
@@ -1,11 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
-import {
-  Ctx,
-  EventPattern,
-  MessagePattern,
-  RmqContext,
-} from '@nestjs/microservices';
+import { Ctx, EventPattern, RmqContext } from '@nestjs/microservices';
 
 @Controller()
 export class InventoryController {
@@ -13,7 +8,6 @@ export class InventoryController {
 
   @EventPattern('check_inventory')
   getOrders(@Ctx() context: RmqContext) {
-    console.log(context.getMessage());
     console.log('processing the message from inventory');
 
     return true;
